b01d5b7ded3a62383eb2985b3157e6016bcf0fe8
Jonzerr <chovanec88@gmail.com>
2023-12-03 21:31:30 +0100

    day04 init

---
+++ src/day04/index.ts
@@ -0,0 +1,9 @@
+import fs from 'fs';
+
+const input :string = fs.readFileSync('src/day04/inputSample.txt', 'utf8');
+const inputArray :string[] = input.split('\n').map((line) => line.trim());
+
+
+console.log("hello world!");
+//console.log('Result of part 1:', resultPart1);
+//console.log('Result of part 2:', resultPart2);
