aacbdebc380f24c732a871f3eac7ae5b4823f53b
Jonzerr <chovanec88@gmail.com>
2023-12-04 12:13:37 +0100

    day04 part1/2

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -1,9 +1,45 @@
 import fs from 'fs';
 
-const input :string = fs.readFileSync('src/day04/inputSample.txt', 'utf8');
+const input :string = fs.readFileSync('src/day04/input.txt', 'utf8');
 const inputArray :string[] = input.split('\n').map((line) => line.trim());
 
+const positionOfDivider = inputArray[0].indexOf('|');
+const scratchcards: number[] = Array(inputArray.length).fill(1);
 
-console.log("hello world!");
-//console.log('Result of part 1:', resultPart1);
-//console.log('Result of part 2:', resultPart2);
+const winningsNumbers = inputArray.map((line) => line
+.slice(line.indexOf(':') + 2, positionOfDivider - 1)
+.split(' ').filter(str => str !== '').map(Number)
+);
+
+const myNumbers = inputArray.map((line) => line
+    .slice(positionOfDivider + 1).split(' ').filter(str => str !== '').map(Number)
+);
+
+let points :number = 0;
+
+for (let i = 0; i < myNumbers.length; i++){
+
+    let winNumbers :number = 0;
+
+    for(let num of myNumbers[i]){
+        if (winningsNumbers[i].includes(num)){
+            winNumbers ++;
+        }
+    }
+
+    // logic part 1
+    if (!(winNumbers === 0)){
+        points += 2 ** (winNumbers - 1);
+    }
+
+    // logic part 2
+    for (let j = i; j < i + winNumbers ; j++){
+        scratchcards[j + 1]  += scratchcards[i];
+    }
+}
+const totalScratchcards = scratchcards.reduce((sum,add)=>sum + add,0);
+
+
+
+console.log('Result of part 1:', points);
+console.log('Result of part 2:', totalScratchcards);
