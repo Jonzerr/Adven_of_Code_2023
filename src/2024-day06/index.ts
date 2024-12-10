086668d8d88883c577bfc7008e92e0adab549d0f
Jonzerr <chovanec88@gmail.com>
2024-12-10 09:02:32 +0100

    2025-day06 initialization

--- src/2024-day06/index.ts
+++ src/2024-day06/index.ts
@@ -2,14 +2,14 @@ import fs from 'fs';
 
 //console.time('executionTime');
 
-const input :string = fs.readFileSync('src/2024-day*/inputSample.txt', 'utf8');
+const input :string = fs.readFileSync('src/2024-day06/inputSample.txt', 'utf8');
 const inputArray :string[] = input.split('\n').map((line) => line.trim());
 
 
 
 
 
-console.log('hello world');
+console.log('helo world');
 //console.log('Result of part 1:', resultPart1);
 //console.log('Result of part 2:', resultPart2);
 
