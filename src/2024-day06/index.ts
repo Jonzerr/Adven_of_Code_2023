3e9796e51bca0ba6005a0cfefb945922d5d17ea5
Jonzerr <chovanec88@gmail.com>
2024-12-11 21:04:42 +0100

    2024-day6-part2

--- src/2024-day06/index.ts
+++ src/2024-day06/index.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
 
-//console.time('executionTime');
+console.time('executionTime');
 
-const input: string = fs.readFileSync('src/2024-day06/inputSample.txt', 'utf8');
+const input: string = fs.readFileSync('src/2024-day06/input.txt', 'utf8');
 const inputArray: string[] = input.split('\n').map((line) => line.trim());
 
 const mapsWidth = inputArray[0].length;
@@ -26,12 +26,12 @@ function turnRight(direction: Direction): Direction {
     return directions[(currentIndex + 1) % 4];
 }
 
-function visitedCount (start: number[]){
-    let [row,col] = start;
+function visitedCount (){
+    let [row,col] = startPosition;
     let directionKey: Direction = 'up';
     const visited = new Set<string>();
     
-    const moves: Record<Direction, [number, number]>={
+    const moves: Record<Direction, [number, number]> = {
         'up': [-1, 0],
         'right': [0, 1],
         'down': [1, 0],
@@ -59,9 +59,71 @@ function visitedCount (start: number[]){
     }
     return visited.size;
 }
+function loopchecker(){
+    let [row,col] = startPosition;
+    let directionKey: Direction = 'up';
+    const visited = new Set<string>();
+    
+    const moves: Record<Direction, [number, number]> = {
+        'up': [-1, 0],
+        'right': [0, 1],
+        'down': [1, 0],
+        'left': [0, -1],
+    }
+
+    while (true) {
+        const step = `${row} ${col} ${directionKey}`;
+        if(visited.has(step)){
+            return true;
+        }
+        visited.add(step);
+        const [dCol, dRow] = moves[directionKey];
+        const nextRow = row + dRow;
+        const nextCol = col + dCol;
+        
+        if (nextRow < 0 || nextRow >= mapsHeight || nextCol < 0 || nextCol >= mapsWidth) {
+            return false;
+        }
+        
+        const nextfield = map[nextCol][nextRow];
+        if ( nextfield === '#'){
+            directionKey = turnRight(directionKey);
+        } else {
+            row = nextRow;
+            col = nextCol;
+        }
+    }
+}
 
 
-console.log('Result of part 1:', visitedCount(startPosition));
-//console.log('Result of part 2:', resultPart2);
+console.log('Result of part 1:', visitedCount());
 
-//console.timeEnd('executionTime'); 
+//Part 2:
+const map = inputArray.map(line => line.split(''));
+
+function countObstructionPosition( start: number[]): number {
+    let loopcount = 0;
+    for (let row = 0; row < mapsWidth; row++) {
+        for (let col = 0; col < mapsHeight; col++) {
+            if (map[row][col] === '.' && !(row === start[0] && col === start[1])) {
+                // Dočasne pridaj prekážku
+                map[row][col] = '#';
+                // if(row === 6 && col === 3){
+                //     console.log( row, col);
+                // }
+                // Spusti simuláciu
+                if (loopchecker()){
+                    loopcount++;
+                }
+                // Vráť mapu do pôvodného stavu
+                map[row][col] = '.';
+            }
+        }
+    }
+    return loopcount;
+}
+
+
+const resultPart2 = countObstructionPosition(startPosition);
+console.log('Result of part 2:', resultPart2);
+console.timeEnd('executionTime'); 
